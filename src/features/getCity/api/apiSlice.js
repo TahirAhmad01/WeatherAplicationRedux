e7cf7e4fc4610bee8b9be94341b0723f3e17ae43
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-10-21 13:21:19 +0600

    minor change

--- src/features/getCity/api/apiSlice.js
+++ src/features/getCity/api/apiSlice.js
@@ -3,11 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.REACT_APP_CITY_NAME_API_URL,
 
-  prepareHeaders: async (headers) => {
-      headers.set("content-Type", "application/json");
-      headers.set("Access-Control-Allow-Origin", "*");
-    return headers;
-  },
+  // prepareHeaders: async (headers) => {
+  //   headers.set("content-Type", "application/json");
+  //   return headers;
+  // },
 });
 
 export const getCityApi = createApi({
